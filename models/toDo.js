9111e1e3fb69004c2a89279d7ef226be6442f8be
osama <enlil96rashid@gmail.com>
2018-02-26 13:43:36 +0100

    add a to-do Schema

---
+++ models/toDo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const toDoSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.ObjectId,
+        required: 'Please Supply a user ID'
+    },
+    item: {
+        type: String,
+        required: 'Please give an item'
+    },
+    display: {
+        type: Boolean,
+        required: 'Please choose a display option'
+    },
+    order: {
+        type: Number,
+        required: 'Please add order to your list.'
+    },
+    
+})
+
+module.exports = mongoose.model('toDo', toDoSchema)
