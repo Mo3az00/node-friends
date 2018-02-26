edb7bc98426d005064279f672ba2ba7853f2d528
mo3az00 <mouaz.meda@hotmail.com>
2018-02-26 14:38:04 +0100

    Renaming SettingsSchema.js to Settings.js and changing module name

--- models/SettingsSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const SettingsSchema = new mongoose.Schema({
-  Tech_Limit: {
-    type: Number
-	}
-})
-
-module.exports = mongoose.model('SettingsSchema', SettingsSchema)
