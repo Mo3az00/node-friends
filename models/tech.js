9e53a95311cfae2def11f4f2cb1429d8fbfb7072
Bjarne <b.rye.soerensen@gmail.com>
2018-02-27 12:00:24 +0100

    commit calender

---
+++ models/tech.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+
+const techSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    trim: true,
+    required: 'Please enter a name!'
+  },
+  title: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  subtitle: {
+    type: {
+      type: String,
+    },
+    description: [{
+      type: String,
+      required: 'You must supply a description!'
+    }],
+    icon: {
+      type: String,
+      required: 'You must supply an icon!'
+    }
+  },
+  order: {
+    type: String,
+    trim: true,
+    required: 'Please enter a name!'
+  }
+
+
+module.exports = mongoose.model('Tech', techSchema);
