0f3efa56bc235f08d023202211a51883734c06d7
majd <majdissa968@gmail.com>
2018-02-27 10:33:03 +0100

    adding the absence schema

---
+++ models/absenceReport.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const absenceReport = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply a user'
+  },
+  form: {
+    type: Date,
+    required: 'You must supply a title',
+    trim: true
+  },
+  untilDate: {
+    type: Date,
+    required: 'You must supply a Date',
+    trim: true
+  },
+  description: {
+    type: String,
+    required: 'You must supply a description',
+    trim: true
+  },
+  photo: {
+    type: String ,
+    required: true
+  }
+}) 
+
+module.exports = mongoose.model('absenceList', absenceReport)
