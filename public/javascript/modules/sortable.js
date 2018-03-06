8b49e6a0a5c7da285a51d3144101811c4807dcab
Dominik Hanke <mail@dominik-hanke.de>
2018-03-06 10:06:01 +0100

    Enhanced sorting

--- public/javascript/modules/sortable.js
+++ public/javascript/modules/sortable.js
@@ -2,32 +2,42 @@ import Sortable from 'sortablejs'
 import axios from 'axios'
 
 function sortable() {
-  Sortable.create(listWithHandle, {
-    group: 'favorites',
-    handle: '.fa.fa-bars',
-    animation: 150,
-    store: {
-      get: function (sortable) {
-        return []
-      },
-      set: function (sortable) {
-        var order = sortable.toArray()
-        sortable.option('disabled', true)
+  $('.table-sortable').each(function() {
+    const table = $(this)
+    const url = table.data('sortable-url') || null
+    const group = table.data('sortable-group') || 'default'
 
-        axios
-          .post('/admin/tech-favorites/update-order', {
-            order
-          })
-          .then((response) => {
-            sortable.option('disabled', false)
-          })
-          .catch((error) => {
-            console.error(error)
-            sortable.option('disabled', false)
-          })
-      }
+    if (url === null) {
+      return
     }
-  });
+
+    Sortable.create(this.querySelector('tbody'), {
+      group,
+      handle: '.sortable-handle',
+      animation: 150,
+      store: {
+        get: function (sortable) {
+          return []
+        },
+        set: function (sortable) {
+          var order = sortable.toArray()
+          sortable.option('disabled', true)
+  
+          axios
+            .post(url, {
+              order
+            })
+            .then((response) => {
+              sortable.option('disabled', false)
+            })
+            .catch((error) => {
+              console.error(error)
+              sortable.option('disabled', false)
+            })
+        }
+      }
+    });
+  })
 }
 
 export default sortable
