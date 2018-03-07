92560db969a65fd0ef482eb636de7d2527eefbef
Dominik Hanke <mail@dominik-hanke.de>
2018-03-07 16:40:16 +0100

    Fixed merge conflicts

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -27,23 +27,19 @@ exports.studentList = async (request, response) => {
 
 // Frontend Page 
 exports.frontendPage = async (request, response) => {
-
-  // Countdown
+  // Calculating days learned and days left
   const now = moment()
+  const courseStart = moment([2017, 9, 4])
   const courseEnd = moment([2018, 8, 16])
+  const daysLearned = now.diff(courseStart, 'days')
   const daysLeft = courseEnd.diff(now, 'days')
 
-  const courseStart = moment([2017, 9, 4])
-  const daysLearned = now.diff(courseStart, 'days')
-
   // Loading data
-
   const technologies = await HomepageTech.find().sort({ 'order': 1 })
-
   const students = await UserProfile.find().populate('user').sort({ 'first_name': 1 })
 
   response.render('layout', {
-    title: 'Main',
+    title: 'We build your next big thing',
     daysLearned,
     daysLeft,
     technologies,
@@ -51,7 +47,7 @@ exports.frontendPage = async (request, response) => {
   })
 }
 
-//studentuserprofile
+// Student profile page
 exports.studentProfile = async (request, response) => {
 
   response.render('studentProfile', {
--- helpers.js
+++ helpers.js
@@ -8,7 +8,7 @@ exports.moment = moment
 
 // Generate an image placeholder
 exports.placeholderImage = function (width, height, icon) {
-    return `
+  return `
     <div class="placeholder-image" style="width: ${width}px; height: ${height}px;">
       <i class="placeholder-image-icon fa fa-${icon}"></i>
     </div>
@@ -22,31 +22,20 @@ exports.navigationMain = [
         href: '/'
     },
     {
-        name: 'Link',
-        href: '#'
+        name: 'About Us',
+        href: '#about-us'
     },
     {
-        name: 'Disabled',
-        href: '#',
-        class: 'disabled'
+        name: 'Tecnologies',
+        href: '#technologies'
     },
     {
-        name: 'Dropdown',
-        href: '#',
-        childs: [
-            {
-                name: 'Action',
-                href: '#'
-            },
-            {
-                name: 'Another Action',
-                href: '#'
-            },
-            {
-                name: 'Something else here',
-                href: '#'
-            },
-        ]
+        name: 'The Team',
+        href: '#the-team'
+    },
+    {
+        name: 'Contact',
+        href: '#contact'
     }
 ]
 
@@ -54,61 +43,61 @@ exports.navigationMain = [
  * DASHBOARD
  */
 exports.adminSidebarNavigation = (user) => {
-    if (!user) {
-        return [
-            {
-                href: '/admin/login',
-                icon: 'sign-in',
-                title: 'Please login'
-            }
-        ]
-    }
+  if (!user) {
+      return [
+          {
+              href: '/admin/login',
+              icon: 'sign-in',
+              title: 'Please login'
+          }
+      ]
+  }
 
-    let navigation = [
-        {
-            href: '/admin',
-            icon: 'th-large',
-            title: 'Dashboard'
-        },
-        {
-            href: '/admin/students',
-            icon: 'users',
-            title: 'Students'
-        },
-        {
-            href: '/admin/absence-reports',
-            icon: 'envelope',
-            title: 'Absence Reports'
-        }
-    ]
+  let navigation = [
+      {
+          href: '/admin',
+          icon: 'th-large',
+          title: 'Dashboard'
+      },
+      {
+          href: '/admin/students',
+          icon: 'users',
+          title: 'Students'
+      },
+      {
+          href: '/admin/absence-reports',
+          icon: 'envelope',
+          title: 'Absence Reports'
+      }
+  ]
+  
+  if (user.role === 'teacher') {
+      navigation = navigation.concat([
+          {
+              href: '/admin/homepage-technologies',
+              icon: 'code',
+              title: 'Technologies'
+          },
+          {
+              href: '#',
+              icon: 'cog',
+              title: 'Settings'
+          },
+      ])
+  } else {
+      navigation = navigation.concat([
+          {
+              href: '/admin/tech-favorites',
+              icon: 'list',
+              title: 'Tech Favorites'
+          },
+          {
+              href: '/admin/projects',
+              icon: 'code',
+              title: 'Projects'
+          },
+      ])
+  }
 
-    if (user.role === 'teacher') {
-        navigation = navigation.concat([
-            {
-                href: '/admin/homepage-technologies',
-                icon: 'code',
-                title: 'Technologies'
-            },
-            {
-                href: '#',
-                icon: 'cog',
-                title: 'Settings'
-            },
-        ])
-    } else {
-        navigation = navigation.concat([
-            {
-                href: '/admin/tech-favorites',
-                icon: 'list',
-                title: 'Tech Favorites'
-            },
-            {
-                href: '/admin/projects',
-                icon: 'code',
-                title: 'Projects'
-            },
-        ])
-    }
-
-    return navigation
+  return navigation
 }
