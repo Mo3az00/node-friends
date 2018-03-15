bb876b1d04289726430c4bbf116b8daa6f0fe1b8
Daniela <daniela.fedyakin@gmail.com>
2018-03-15 14:15:05 +0100

    removed test

--- routes/web.js
+++ routes/web.js
@@ -186,12 +186,5 @@ router.get('/admin/settings', catchErrors(SettingsController.form))
 router.post('/admin/settings', catchErrors(SettingsController.updateSettings))
 
 
-// TEST FOR EMAIL
-
-router.get('/emailSent', (request, response) => {
-  response.render('email/password-reset')
-})
-
-
 // Export our router
 module.exports = router;
