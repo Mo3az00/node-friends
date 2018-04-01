a35e4c46bdafbef3438f5b00861fd2a4f17e71e0
Dominik Hanke <mail@dominik-hanke.de>
2018-04-01 19:35:11 +0200

    Cleanup and optimizations

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -18,8 +18,6 @@ exports.catchErrors = (fn) => {
   If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
 */
 exports.notFound = (req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
   res.render('404', {
     title: 'Error 404',
     bodyClass: 'scrolled'
--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 
 // import environmental variables
 require('dotenv').config({ path: 'variables.env' })
@@ -18,71 +19,75 @@ if (process.env.DATABASE_USERNAME
   }
 }
 
-mongoose.Promise = global.Promise
-mongoose.connect(
-  `mongodb://${process.env.DATABASE_HOST || localhost}:${process.env.DATABASE_PORT || 27017}/${process.env.DATABASE_NAME || 'express-mongo-boilerplate'}`,
-  mongooseOptions
-)
-  .catch((error) => {
-    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${error.message}`)
-    process.exit(1)
-  })
+(async () => {
+  const connection = await mongoose
+    .connect(
+      `mongodb://${process.env.DATABASE_HOST || localhost}:${process.env.DATABASE_PORT || 27017}/${process.env.DATABASE_NAME || 'express-mongo-boilerplate'}`,
+      mongooseOptions
+    )
+    .catch((error) => {
+      console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${error.message}\n`)
+    })
 
-// load all models
-require('./models/Settings')
-require('./models/HomepageTech')
-require('./models/ToDo')
-require('./models/User')
-require('./models/UserProject')
-require('./models/UserTechFavorite')
-require('./models/AbsenceReport')
-
-
-// load the app
-const app = require('./app')
-
-// define SSL/TLS options
-let tlsEnabled = false
-let tlsOptions = {}
-
-if (process.env.SSL === 'on' &&
-  process.env.SSL_CERT != undefined &&
-  process.env.SSL_KEY != undefined &&
-  process.env.SSL_CERT != '' &&
-  process.env.SSL_KEY != '') {
-  tlsEnabled = true
-
-  try {
-    tlsOptions = {
-      key: fs.readFileSync(process.env.SSL_KEY),
-      cert: fs.readFileSync(process.env.SSL_CERT)
-    }
-
-    if (process.env.SSL_CHAIN != undefined &&
-      process.env.SSL_CHAIN != '') {
-      tlsOptions.ca = fs.readFileSync(process.env.SSL_CHAIN)
-    }
-
-    if (process.env.SSL_DHPARAM != undefined &&
-      process.env.SSL_DHPARAM != '') {
-      tlsOptions.dhparam = fs.readFileSync(process.env.SSL_DHPARAM)
-    }
-  } catch (e) {
-    console.error(`\n!!! ${e.message}\n`)
-    console.error('=> SSL could not be enabled. Using fallback.\n')
-    tlsEnabled = false
+  if (!connection) {
+    process.exit(0)
   }
-}
 
-// start the app
-app.set('port', process.env.PORT || 7777)
-
-if (tlsEnabled === true) {
-  const server = https.createServer(tlsOptions, app).listen(app.get('port'), () => {
-    console.log(`Express running with TLS → PORT ${server.address().port}`)
-  })
-} else {
-  const server = app.listen(app.get('port'), () => {
-    console.log(`Express running → PORT ${server.address().port}`)
-  })
-}
+  // load all models
+  require('./models/Settings')
+  require('./models/HomepageTech')
+  require('./models/ToDo')
+  require('./models/User')
+  require('./models/UserProject')
+  require('./models/UserTechFavorite')
+  require('./models/AbsenceReport')
+  
+  // load the app
+  const app = require('./app')
+  
+  // define SSL/TLS options
+  let tlsEnabled = false
+  let tlsOptions = {}
+  
+  if (process.env.SSL === 'on' &&
+    process.env.SSL_CERT != undefined &&
+    process.env.SSL_KEY != undefined &&
+    process.env.SSL_CERT != '' &&
+    process.env.SSL_KEY != '') {
+    tlsEnabled = true
+  
+    try {
+      tlsOptions = {
+        key: fs.readFileSync(process.env.SSL_KEY),
+        cert: fs.readFileSync(process.env.SSL_CERT)
+      }
+  
+      if (process.env.SSL_CHAIN != undefined &&
+        process.env.SSL_CHAIN != '') {
+        tlsOptions.ca = fs.readFileSync(process.env.SSL_CHAIN)
+      }
+  
+      if (process.env.SSL_DHPARAM != undefined &&
+        process.env.SSL_DHPARAM != '') {
+        tlsOptions.dhparam = fs.readFileSync(process.env.SSL_DHPARAM)
+      }
+    } catch (e) {
+      console.error(`\n!!! ${e.message}\n`)
+      console.error('=> SSL could not be enabled. Using fallback.\n')
+      tlsEnabled = false
+    }
+  }
+  
+  // start the app
+  app.set('port', process.env.PORT || 7777)
+  
+  if (tlsEnabled === true) {
+    const server = https.createServer(tlsOptions, app).listen(app.get('port'), () => {
+      console.log(`Express running with TLS → PORT ${server.address().port}`)
+    })
+  } else {
+    const server = app.listen(app.get('port'), () => {
+      console.log(`Express running → PORT ${server.address().port}`)
+    })
+  }
+})()
